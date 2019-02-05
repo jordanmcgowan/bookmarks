f1a10c0e33e932c990a4db7a2b0da72a97b96396
Jordan McGowan <jordanmcgowan@users.noreply.github.com>
2019-02-05 11:39:00 -0600

    Adds support to edit rating as well as style updates (#2)

--- app/src/main/java/com/bookmarks/EditBookmarkActivity.kt
+++ app/src/main/java/com/bookmarks/EditBookmarkActivity.kt
@@ -10,6 +10,7 @@ import kotlinx.android.synthetic.main.activity_edit_bookmark.editAuthorET
 import kotlinx.android.synthetic.main.activity_edit_bookmark.editBookmarkButton
 import kotlinx.android.synthetic.main.activity_edit_bookmark.editIsbnET
 import kotlinx.android.synthetic.main.activity_edit_bookmark.editPagesReadET
+import kotlinx.android.synthetic.main.activity_edit_bookmark.editRatingET
 import kotlinx.android.synthetic.main.activity_edit_bookmark.editTitleET
 
 class EditBookmarkActivity : AppCompatActivity() {
@@ -32,10 +33,11 @@ class EditBookmarkActivity : AppCompatActivity() {
   }
 
   fun setupBookInteractions() {
-    editIsbnET.hint = bookmark!!.isbn.toString()
-    editTitleET.hint = bookmark!!.title
-    editAuthorET.hint = bookmark!!.author
+    editIsbnET.hint = getString(R.string.isbn_prefix, bookmark!!.isbn)
+    editTitleET.hint = getString(R.string.title_prefix, bookmark!!.title)
+    editAuthorET.hint = getString(R.string.author_prefix, bookmark!!.author)
     editPagesReadET.hint = getString(R.string.pages_read, bookmark!!.numPages)
+    editRatingET.hint = getString(R.string.rating_prefix, bookmark!!.rating)
   }
 
   fun editBookmark() {
@@ -59,8 +61,14 @@ class EditBookmarkActivity : AppCompatActivity() {
     } else {
       bookmark!!.pagesRead
     }
+    val rating = if (!editRatingET.text.isNullOrEmpty()) {
+      editRatingET.text.toString().toDoubleOrNull()
+    } else {
+      bookmark!!.rating
+    }
 
-    val newBookmark = bookmark!!.copy(isbn = isbn, author = author, title = title, pagesRead = pagesRead ?: 0)
+
+    val newBookmark = bookmark!!.copy(isbn = isbn, author = author, title = title, pagesRead = pagesRead ?: 0, rating = rating?: 0.0)
     if (bookmark!!.isbn == isbn) {
       bookmarkListViewModel.updateBookmark(newBookmark)
       Toast.makeText(this, "Updated entry for ${bookmark!!.title}", Toast.LENGTH_SHORT).show()
